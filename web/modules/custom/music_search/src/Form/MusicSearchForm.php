3490e554457a8fa1ed385d33eeb0569c708fd4ab
kristofer <kristofergauti@gmail.com>
2021-05-09 19:27:31 +0000

    Form added with respect to redirecting to the search results page

--- web/modules/custom/music_search/src/Form/MusicSearchForm.php
+++ web/modules/custom/music_search/src/Form/MusicSearchForm.php
@@ -1,59 +1,52 @@
 <?php
 namespace Drupal\music_search\Form;
 
-use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
 
-class MusicSearchForm extends ConfigFormBase {
+class MusicSearchForm extends FormBase {
 
   /**
    * {@inheritDoc}
    */
-  protected function getEditableConfigNames()
-  {
+  protected function getEditableConfigNames() {
     return ["music_search.search"];
   }
 
   /**
    * {@inheritDoc}
    */
-  public function getFormId()
-  {
+  public function getFormId() {
     return "music_search_form";
   }
 
   /**
    * {@inheritDoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state)
-  {
-    $config = $this->config("music_search.search");
-    $form["music_search"] = [
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form["Album"] = [
       "#type" => "textfield",
-      "#title" => $this->t("Music Search"),
-      "#description" => $this->t("Please provide the song that you want to search for!"),
-      "#default value" => $config->get("salutation")
+      "#title" => $this->t("Album"),
+      "#description" => $this->t("Please provide the album name that you want to search for!"),
     ];
-    return parent::buildForm($form, $form_state);
+    $form["Artist"] = [
+      "#type" => "textfield",
+      "#title" => $this->t("Artist"),
+      "#description" => $this->t("Please provide the artist name that you want to search for!"),
+    ];
+    $form["Search"] = [
+      "#type" => "submit",
+      "#value" => "Search"
+    ];
+    return $form;
   }
 
   /**
    * {@inheritDoc}
    */
-  public function submitForm(array &$form, FormStateInterface $form_state)
-  {
-    $this->config("music_search.search")
-      ->set("music_search", $form_state->getValue("music_search"))
-      ->save();
-    parent::submitForm($form, $form_state);
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $form_state->setRedirectUrl(Url::fromUri('internal:/search_results'));
   }
 
-  public function validateForm(array &$form, FormStateInterface $form_state)
-  {
-    $salutation = $form_state->getValue("music_search");
-    if (strlen($salutation) > 20) {
-      $form_state->setErrorByName("music_search", $this->t("This music search is too long"));
-    }
-    parent::validateForm($form, $form_state);
-  }
 }
