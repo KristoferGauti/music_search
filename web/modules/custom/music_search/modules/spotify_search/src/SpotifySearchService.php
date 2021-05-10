d20869550164041619fd2ed111727ad522f383a4
Eggert Eggertsson <eggertsigtryggsson@Eggerts-MacBook-Pro.local>
2021-05-10 12:39:54 +0000

    hello

--- web/modules/custom/music_search/modules/spotify_search/src/SpotifySearchService.php
+++ web/modules/custom/music_search/modules/spotify_search/src/SpotifySearchService.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\music_search\modules\spotify_search;
 
+use http\Message\Body;
+
 class SpotifySearchService {
   /**
    * Sends a GET query to Spotify for specific URL
@@ -12,78 +14,84 @@ class SpotifySearchService {
    *   Returns a stdClass with the search results or an error message
    */
   function _spotify_api_get_query($uri) {
-    $cache = _spotify_api_get_cache_search($uri);
+//    $cache = $this->_spotify_api_get_cache_search($uri);
     $search_results = null;
+//
+//    if (!empty($cache)) {
+//      $search_results = $cache;
+//    }
+//    else {
+    $token = $this->_spotify_api_get_auth_token();
+    $token = json_decode($token);
+    $options = array(
+      'method' => 'GET',
+      'timeout' => 3,
+      'headers' => array(
+        'Accept' => 'application/json',
+        'Authorization' => "Bearer " . $token->access_token,
+      ),
+    );
 
-    if (!empty($cache)) {
-      $search_results = $cache;
-    }
-    else {
-      $token = _spotify_api_get_auth_token();
-      $token = json_decode($token);
-      $options = array(
-        'method' => 'GET',
-        'timeout' => 3,
-        'headers' => array(
-          'Accept' => 'application/json',
-          'Authorization' => "Bearer " . $token->access_token,
-        ),
-      );
+      $client =  \Drupal::httpClient();
+      $response = $client -> get($uri, $options);
+      #$search_results = $response->getBody();     #data
 
-      $search_results = drupal_http_request($uri, $options);
+
 
       if (empty($search_results->error)) {
-        $search_results = drupal_json_decode($search_results->data);
-        _spotify_api_set_cache_search($uri, $search_results);
+        $search_results = json_decode($response->getBody());
+        //$this->_spotify_api_set_cache_search($uri, $search_results);
 
       }
       else {
-        drupal_set_message(t('The search request resulted in the following error: @error.', array(
-          '@error' => $search_results->error,
-        )));
+//        drupal_set_message(t('The search request resulted in the following error: @error.', array(
+//          '@error' => $response->error,
+//        )));
 
+
+        \Drupal::messenger()->addMessage(t('The search request resulted in the following error: @error', array('@error' => $response->error,
+          )), 'error');
         return $search_results->error;
       }
-    }
-
     return $search_results;
   }
 
-  /**
-   * Saves a search to Drupal's internal cache.
-   *
-   * @param string $cid
-   *   The cache id to use.
-   * @param array $data
-   *   The data to cache.
-   */
-  function _spotify_api_set_cache_search($cid, array $data) {
-    cache_set($cid, $data, 'spotify-api-cache', time() + SPOTIFY_CACHE_LIFETIME);
-  }
-
-  /**
-   * Looks up the specified cid in cache and returns if found
-   *
-   * @param string $cid
-   *   Normally a uri with a search string
-   *
-   * @return array|bool
-   *   Returns either the cache results or false if nothing is found.
-   */
-  function _spotify_api_get_cache_search($cid) {
-    $cache = cache_get($cid, 'spotify-api-cache');
-    if (!empty($cache)) {
-      if ($cache->expire > time()) {
-        return $cache->data;
-      }
-    }
-    return FALSE;
-  }
+//  /**
+//   * Saves a search to Drupal's internal cache.
+//   *
+//   * @param string $cid
+//   *   The cache id to use.
+//   * @param array $data
+//   *   The data to cache.
+//   */
+//  function _spotify_api_set_cache_search($cid, array $data) {
+//    cache_set($cid, $data, 'spotify-api-cache', time() + SPOTIFY_CACHE_LIFETIME);
+//  }
+//
+//  /**
+//   * Looks up the specified cid in cache and returns if found
+//   *
+//   * @param string $cid
+//   *   Normally a uri with a search string
+//   *
+//   * @return array|bool
+//   *   Returns either the cache results or false if nothing is found.
+//   */
+//  function _spotify_api_get_cache_search($cid) {
+//    $cache = cache_get($cid, 'spotify-api-cache');
+//    if (!empty($cache)) {
+//      if ($cache->expire > time()) {
+//        return $cache->data;
+//      }
+//    }
+//    return FALSE;
+//  }
 
   /**
    * Gets Auth token from the Spotify API
    */
-  function _spotify_api_get_auth_token() {
+
+  private function _spotify_api_get_auth_token() {
     $SPOTIFY_API_CLIENT_ID = "529fd7ae993c488383c2700160208bbf";
     $SPOTIFY_API_CLIENT_SECRET = "7a1c8b560e5b426b978963b897a1b6a7";
     $connection_string = "https://accounts.spotify.com/api/token";
